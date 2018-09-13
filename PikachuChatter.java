77c68080f0553f4fe2aa518a05427bdcb0dd13c2
yourgirlfriend <cmsmith2067@gmail.com>
2018-09-13 08:02:24 -0700

    Add files via upload

---
+++ PikachuChatter.java
@@ -0,0 +1,22 @@
+
+public class PikachuChatter {
+	// This is the intro to the class
+	public static void main(String[] args) {
+		// This is the simple print out that makes the code
+		System.out.println("Pika Pika Pika chu Pika chu peeeee ka pika chu!");
+		Pikachu();
+		System.out.println("Pika? Pika Pika Pika chu peeeee ka chu!");
+		Pikachu();
+		
+
+	}
+	
+	// THis is the method for Pikachu that is reused
+	public static void Pikachu()
+	{
+		System.out.println("(\\__/)\n" +
+				 "(0^.^)\")\n" +
+				"z(_(\")(\")\n");
+	}
+
+}
